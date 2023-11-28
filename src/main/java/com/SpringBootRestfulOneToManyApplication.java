c49aa36d241267f94de80d4c431726ac4c96d6a6
BigBeefs <bigbeefs98@gmail.com>
2023-11-28 11:37:27 +0100

    Aggiorno il master

--- src/main/java/com/SpringBootRestfulOneToManyApplication.java
+++ src/main/java/com/SpringBootRestfulOneToManyApplication.java
@@ -1,13 +1,20 @@
 package com;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootRestfulOneToManyApplication {
+public class SpringBootRestfulOneToManyApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootRestfulOneToManyApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+
 	}
 
 }
